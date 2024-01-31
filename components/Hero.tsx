c22e020d8dd9fc2dcd1b37f6afa69a69edce6dcb
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-01-31 22:50:39 +0300

    refactors

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,21 +3,27 @@ import Image from "next/image";
 function Hero() {
   return (
     <header className="bg-hero bg-center bg-cover bg-no-repeat sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
-      <div className="flex-1 flex flex-col gap-10">
+      <div className="flex-1 flex flex-row justify-center  gap-5">
         <Image
-          src="./logo.svg"
-          alt="logo"
+          src=""
+          alt=""
           width={101}
           height={96}
           className="object-contain"
         />
-        <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
-          Explore The <span className="red-gradient">Diverse Realms</span> of
-          Anime Magic
+        <h1 className="sm:text-6xl   text-5xl text-blue-500 lg:max-w-lg font-bold leading-[120%]">
+          Dive into a set of
+          <span className="text-teal-500">
+            {" "}
+            Animations in<span className="text-indigo-600 m-2 ">
+              Anime
+            </span>{" "}
+            World
+          </span>
         </h1>
       </div>
       <div className="lg:flex-1 relative w-full h-[50vh] justify-center">
-        <Image src="/anime.png" alt="anime" fill className="object-contain" />
+        {/* <Image src="/anime.png" alt="anime" fill className="object-contain" /> */}
       </div>
     </header>
   );
