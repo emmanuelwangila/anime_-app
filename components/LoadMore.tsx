0abc8448cfdb7b4f4863dc7756156de5e1750064
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-01-31 20:18:48 +0300

    feat fix animation scroll complete

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,19 +1,29 @@
 "use client";
+import { fetchAnimation } from "@/app/action";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
+import { AnimeProp } from "./AnimeCard";
+import AnimeCard from "./AnimeCard";
 
 function LoadMore() {
   const { ref, inView } = useInView();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<AnimeProp[]>([]);
 
   useEffect(() => {
     if (inView) {
-      alert("load more animes");
+      fetchAnimation(2).then((res) => {
+        setData([...data, ...res]);
+      });
     }
-  }, [inView]);
+  }, [inView, data]);
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
           <Image
