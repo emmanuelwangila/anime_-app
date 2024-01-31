573395a554a3f6790e23f6481c35ffaaaf006339
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-01-31 17:20:56 +0300

    init anime app

---
+++ app/action.ts
@@ -0,0 +1,10 @@
+"use server";
+
+const fetchAnimation = async () => {
+  const response = await fetch("https:shikimori/.one/apis/animes");
+
+  const data = await response.json();
+
+  console.log(data);
+  return data;
+};
